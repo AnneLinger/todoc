a566a195346cc7c50cc572c42343c195b486556d
Anne <anne-linger@orange.fr>
2022-03-04 08:44:53 +0100

    Add TodocDatabase

---
+++ app/src/main/java/com/cleanup/todoc/database/TodocDatabase.java
@@ -0,0 +1,37 @@
+package com.cleanup.todoc.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.cleanup.todoc.database.dao.ProjectDao;
+import com.cleanup.todoc.database.dao.TaskDao;
+import com.cleanup.todoc.model.Project;
+import com.cleanup.todoc.model.Task;
+
+/**
+*Abstract class to instance the Room db
+*/
+@Database(entities = {Task.class, Project.class}, version = 1, exportSchema = false)
+public abstract class TodocDatabase extends RoomDatabase {
+    //Singleton
+    private static volatile TodocDatabase INSTANCE;
+
+    //Dao
+    public abstract TaskDao mTaskDao();
+    public abstract ProjectDao mProjectDao();
+
+    //Instance
+    public static TodocDatabase getInstance(Context context) {
+        if(INSTANCE == null) {
+            synchronized (TodocDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), TodocDatabase.class, "TodocDatabase.db").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
